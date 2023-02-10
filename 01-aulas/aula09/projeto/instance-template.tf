3a7a899f4b0388d3d07a93204b8b7cc61535c59a
Anderson Bispo <absilva@tjba.jus.br>
2023-02-10 20:27:46 -0300

    aula09-projeto

---
+++ 01-aulas/aula09/projeto/instance-template.tf
@@ -0,0 +1,27 @@
+resource "google_compute_instance_template" "default" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  tags = ["webapp"]
+
+  instance_description = "Instancia criada via template"
+  machine_type         = var.default_vm_size
+
+  // Create a new boot disk from an image
+  disk {
+    source_image      = var.default_vm_image
+    auto_delete       = true
+    boot              = true
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.self_link
+
+    access_config {
+      
+    }
+  }
+
+  metadata_startup_script = file(var.prov_script)
+
+}
---
+++ 01-aulas/aula09/projeto/variables.tf
@@ -0,0 +1,68 @@
+// variaveis de rede
+
+variable "vpc_name" {
+  description = "Nome da VPC"
+  type        = string
+  default     = "vpc-network"
+}
+
+variable "option_auto_create" {
+  description = "Define se devem ser criadas as subnets automaticamente"
+  type        = bool
+  default     = "true"
+}
+
+//valores default
+
+variable "default_project" {
+  description = "Nome do Projeto"
+  type        = string
+  default     = "lab-terraform-8249-376522"
+}
+
+variable "default_vm_size" {
+  description = "Size padrão das VMs"
+  type        = string
+  default     = "e2-small"
+}
+
+variable "default_vm_image" {
+  description = "Imagem padrão das VMs"
+  type        = string
+  default     = "debian-cloud/debian-11"
+}
+
+
+
+// variaveis de processamento
+
+variable "prov_script" {
+  description = "Script de provisionamento"
+  type        = string
+  default     = "./scripts/prov-back.sh"
+}
+
+variable "web1_vm_name" {
+  description = "Nome da vm de web"
+  type        = string
+  default     = "web1"
+}
+
+variable "web1_vm_zone" {
+  description = "Zona da vm de web"
+  type        = string
+  default     = "us-central1-a"
+}
+
+
+variable "web2_vm_name" {
+  description = "Nome da vm de web"
+  type        = string
+  default     = "web2"
+}
+
+variable "web2_vm_zone" {
+  description = "Zona da vm de web"
+  type        = string
+  default     = "us-central1-b"
+}
